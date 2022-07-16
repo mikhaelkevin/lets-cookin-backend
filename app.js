5bbc4cecac1fbd5b1357bfbfb6f06ff152e3834d
Mikhael <goodboybutnot@gmail.com>
2022-07-16 15:11:18 +0700

    test

--- app.js
+++ app.js
@@ -41,6 +41,10 @@ app.use('/letscookinapps/', cors(corsOptionsDelegate), usersRoutes);
 app.use('/letscookinapps/', cors(corsOptionsDelegate), recipesRoutes);
 app.use('/letscookinapps/', authRoutes);
 
+app.use('*', (req, res) => {
+  res.send('Sukses');
+});
+
 // Error Handler
 app.use(errorHandler);
 
