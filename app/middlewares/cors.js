f59f8bf1f55983ccbcaa8843c750b9e687d4aa31
Mikhael <goodboybutnot@gmail.com>
2022-07-16 12:20:24 +0700

    change cors setting

--- app/middlewares/cors.js
+++ app/middlewares/cors.js
@@ -1,9 +1,14 @@
 const cors = require('cors');
 
-const corsOptions = {
-  origin: '*',
-  optionsSuccessStatus: 200,
-  head: 'Content-Type: application/json'
+const allowlist = ['https://letscookin-app.web.app'];
+const corsOptionsDelegate = function (req, callback) {
+  let corsOptions;
+  if (allowlist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
+  }
+  callback(null, corsOptions); // callback expects two parameters: error and options
 };
 
-module.exports = cors(corsOptions);
+module.exports = cors(corsOptionsDelegate);
