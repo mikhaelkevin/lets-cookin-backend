db1c8f90282aae622d6bf4485c4a214c22829227
Mikhael <goodboybutnot@gmail.com>
2022-07-28 12:38:16 +0700

    add cloud storage for user profile on edit profile

--- app/controllers/usersControllers.js
+++ app/controllers/usersControllers.js
@@ -6,6 +6,7 @@ const { ErrorResponse } = require('../../utils/errorResponse');
 const { getRecipeByIdModel } = require('../models/Recipe');
 const { addCommentModel } = require('../models/Comment');
 const { addUserModel, getUserByEmailModel, getAllUsersModel, getUserProfileModel, deleteUserModel, editUserModel } = require('../models/User');
+const cloudinary = require('../../utils/cloudinary');
 
 const addUser = async (req, res) => {
   console.log('req.body', req.body); const { email, phoneNumber, password, name } = req.body;
@@ -58,18 +59,31 @@ const editUser = async (req, res) => {
 
   const getByEmailResult = await getUserByEmailModel(email);
   const duplicateEmailStatus = (email === getByEmailResult?.[0]?.email);
-  if (duplicateEmailStatus) throw new ErrorResponse('Email has been used!', 400);
+  if (duplicateEmailStatus.length) throw new ErrorResponse('Email has been used!', 400);
 
   const phoneNumberNotValid = (phoneNumber && isNaN(Number(phoneNumber))) || (phoneNumber && phoneNumber.length < 11);
   if (phoneNumberNotValid) throw new ErrorResponse('Phonenumber format must be a number and at least have more than 11 digits');
 
   const userData = await getUserProfileModel(id);
+  let profilePicturePath = req?.file?.path || userData.profile_picture;
+  let profilePictureId = userData.profile_picture_id;
+
+  if (profilePicturePath) {
+    if (profilePictureId) {
+      await cloudinary.uploader.destroy(profilePictureId);
+    }
+    const cloudUpload = await cloudinary.uploader.upload(profilePicturePath);
+    profilePicturePath = cloudUpload.secure_url;
+    profilePictureId = cloudUpload.public_id;
+  }
+
   const tempUserProfileData = {
     id,
     name: name || userData.name,
     email: email || userData.email,
     phoneNumber: phoneNumber || userData.phonenumber,
-    profilePicture: req?.file?.path || userData.profile_picture
+    profilePicturePath,
+    profilePictureId
   };
 
   const editUserResult = await editUserModel(tempUserProfileData);
