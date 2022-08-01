0f5f20c8288a774a2a64cf590d2d12e43a3c9fd8
Mikhael <goodboybutnot@gmail.com>
2022-08-01 19:15:23 +0700

    cors update

--- app.js
+++ app.js
@@ -21,10 +21,11 @@ const { errorHandler } = require('./app/middlewares/errorHandler');
 const cors = require('cors');
 
 const allowlist = [
-  'https://letscookin-app.web.app',
-  'http://localhost:3001',
-  'http://localhost:3000',
-  'https://next-letscookin-apps-r6nwuwmky-mikhaelkevin.vercel.app/'
+  // 'https://letscookin-app.web.app',
+  // 'http://localhost:3001',
+  // 'http://localhost:3000',
+  // 'https://next-letscookin-apps-r6nwuwmky-mikhaelkevin.vercel.app/'
+  '*'
 ];
 const corsOptionsDelegate = function (req, callback) {
   let corsOptions;
