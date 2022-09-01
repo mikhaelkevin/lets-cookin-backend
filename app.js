4e65a555a1c32981338367a135e9fcbc33f0c6d4
Mikhael <goodboybutnot@gmail.com>
2022-09-02 04:28:23 +0700

    cors update on auth

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const helmet = require('helmet');
-const restify = require('restify');
 const xss = require('xss-clean');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
-const appXSS = restify.createServer();
 const app = express();
 const port = process.env.PORT || process.env.LOCAL_PORT;
 
 // Server Add-on
-appXSS.use(restify.bodyParser());
 app.use(helmet({ crossOriginResourcePolicy: false }));
 app.use(bodyParser.json());
 
@@ -43,7 +40,7 @@ const recipesRoutes = require('./routes/recipesRoutes');
 const authRoutes = require('./routes/auth');
 
 // Routes Endpoint
-appXSS.use(xss());
+app.use(xss());
 app.use('/letscookinapps/', cors(corsOptionsDelegate), usersRoutes);
 app.use('/letscookinapps/', cors(corsOptionsDelegate), recipesRoutes);
 app.use('/letscookinapps/', cors(corsOptionsDelegate), authRoutes);
